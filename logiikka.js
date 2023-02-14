2f982dde794c68c921c1e2ddebba670a3382540e
Niroasd <123634396+Niroasd@users.noreply.github.com>
2023-02-14 05:10:11 +0200

    Kielivaihto nappi.
    kielivaihto tehty. lisätty joka html filelle natiivi versio jonka lataa ja sisällöt käännetty. js jätetty tulevaisuuden varalle.

--- logiikka.js
+++ logiikka.js
@@ -1,5 +1,10 @@
-let kieli;
-
-function kieliVaihto(){
+/* function kieliVaihto(){
     console.log("Kieli Vaihdettu");
 }
+
+function getKieli(){
+    var currentLoc = window.location;
+    console.log(currentLoc);
+} */
+
+// löytyki simppelimpi tapa tehä kielenvaihto, olen idiootti :)
